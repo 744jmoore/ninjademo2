340dcb61898bdef6f76793233d150e547d6d01cf
Jacob-Moore_lmig <Jacob.Moore@LibertyMutual.com>
2023-05-30 08:39:19 -0400

    added State and blog list with filter

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,19 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
 
-   const [name, setName] = useState('mario') ;
-   const[age, setAge]= useState(30);
-    
-    const handleClick =() =>{
-        setName('luigi');
-        setAge(55);
-    }
-
-    // const handleClickAgain = (name, e) => {
-    //     console.log("hello "  + name, e.timeStamp);
-    // }
-     
+   const [blogs, setBlogs] = useState([
+      {title: 'html', body: 'some html content here', author: 'Jake', id: 1},
+      {title: 'css', body: 'some css content here', author: 'Jeff', id: 2},
+      {title: 'javascript', body: 'some javascript content here', author: 'Jake', id: 3}
+   ]);
     
       return ( 
          <div className="home">
-            <h2>HomePage</h2>
-            <p>{ name } is { age } years old!</p>
-            <button onClick={handleClick}>Click Me</button>
-             {/* <button onClick={(e) => {handleClickAgain('Mario', e)}}>Click Again</button> */}
+               <BlogList blogs={blogs} title="All Blogs!"/>
+               <BlogList blogs={blogs.filter((blogs)=> blogs.author==="Jeff")} title="Jeff's Blogs!"/>
         </div>
      );
 }
